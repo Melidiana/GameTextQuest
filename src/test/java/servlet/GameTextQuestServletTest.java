49a2ef87fa20ff31c9461a62d89fdd1f35bca7a2
diana <diana.r.tretyakova@gmail.com>
2023-07-23 22:30:55 +0500

    tests have been added for servlet

--- src/test/java/servlet/GameTextQuestServletTest.java
+++ src/test/java/servlet/GameTextQuestServletTest.java
@@ -1,10 +1,13 @@
 package servlet;
 
+import model.GameTextQuestResult;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import service.GameTextQuestService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,8 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class GameTextQuestServletTest {
@@ -31,10 +33,14 @@ class GameTextQuestServletTest {
     @Mock
     HttpSession session;
 
+    @Mock
+    GameTextQuestService gameTextQuestService;
+
     @InjectMocks
     GameTextQuestServlet gameTextQuestServlet;
 
     @Test
+    @DisplayName("Test")
     void doGet() throws ServletException, IOException {
         when(request.getSession()).thenReturn(session);
         when(request.getRequestDispatcher("welcome.jsp")).thenReturn(dispatcher);
@@ -48,14 +54,52 @@ class GameTextQuestServletTest {
     }
 
     @Test
-    void doPost() throws ServletException, IOException {
-        when(request.getSession()).thenReturn(session);
+    @DisplayName("Test that enterName.jsp has been redirected one time")
+    public void testEnterNameJspRedirect() throws Exception {
         when(request.getParameter("action")).thenReturn("startGame");
 
         gameTextQuestServlet.doPost(request, response);
 
-        verify(request).getParameter("action");
-        verify(response).sendRedirect("enterName.jsp");
+        verify(response, times(1)).sendRedirect("enterName.jsp");
+    }
 
+    @Test
+    @DisplayName("Test that dispatcher works correctly")
+    public void testDoPostStartAgain() throws Exception {
+        when(request.getSession()).thenReturn(session);
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        when(request.getParameter("action")).thenReturn(null);
+        when(request.getParameter("playerName")).thenReturn("Diana");
+        when(request.getParameter("actionAgain")).thenReturn("startAgain");
+
+        gameTextQuestServlet.doPost(request, response);
+
+        verify(dispatcher).forward(request, response);
+    }
+
+    @Test
+    @DisplayName("Test")
+    public void testDoPostPlayGame() throws Exception {
+        when(request.getSession()).thenReturn(session);
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        when(request.getParameter("action")).thenReturn("playGame");
+        when(session.getAttribute("playerName")).thenReturn("Diana");
+        when(request.getParameter("playerName")).thenReturn("Diana");
+        when(session.getAttribute("gamesPlayed")).thenReturn(1);
+        when(session.getAttribute("currentName")).thenReturn("Diana");
+        when(request.getParameter("actionAgain")).thenReturn(null);
+        when(request.getParameter("answer")).thenReturn("answer");
+
+        GameTextQuestResult result = new GameTextQuestResult();
+        result.setMessage("Your action was successful.");
+        when(gameTextQuestService.processPlayerAction(anyString())).thenReturn(result);
+
+        gameTextQuestServlet.doPost(request, response);
+
+        verify(request).setAttribute("message", result.getMessage());
+        verify(request).setAttribute("options", result.getOptions());
+        verify(request).setAttribute("victory", result.isVictory());
+        verify(request).setAttribute("loss", result.isLoss());
+        verify(dispatcher).forward(request, response);
     }
 }
